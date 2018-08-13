309305e7d584a36ace7e360c16730d479116ac23
Vika Kapitulina <vika.kapitulina@gmail.com>
2018-08-14 01:24:15 +0300

    Implement routing

---
+++ resources/assets/js/routes/index.js
@@ -0,0 +1,18 @@
+import Dashboard from '../components/Dashboard.vue'
+import Event from '../components/Event.vue'
+import Register from '../components/Register.vue'
+
+export const routes = [
+	{
+		path: '/',
+		component: Dashboard
+	},
+	{
+		path: '/event/:id',
+		component: Event
+	},
+	{
+		path: '/event/:id/register',
+		component: Register
+	}
+]
